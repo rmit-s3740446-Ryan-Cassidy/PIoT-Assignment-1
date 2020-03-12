3a891f7a7229acd61ca81f7dbc7d01e2f4ae96b5
rmit-s3740446-Ryan-Cassidy <37936511+rmit-s3740446-Ryan-Cassidy@users.noreply.github.com>
2020-03-13 00:43:06 +1100

    csv appending

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
 import electronicDie
 import csv
 import os
+import datetime
 
 sense = SenseHat()
 #Electronic die instance
@@ -20,6 +21,7 @@ if os.path.exists(filename) == False:
 #Players
 p1 = 0
 p2 = 0
+winner = None
 
 sense.show_message("Game")
 while True:
@@ -33,4 +35,13 @@ while True:
     print(p2)
     if p2 >= 30:
         break
+if p1 > p2:
+    winner = 'p1'
+    sense.show_message("P1 is the winner")
+else:
+    sense.show_message("P2 is the winner")
+with open(filename, 'a') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow([winner, p1, p2, datetime.datetime.now()])
 sense.show_message("End")
