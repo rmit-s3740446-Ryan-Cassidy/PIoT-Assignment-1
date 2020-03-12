963f6be2c78075b7f2d7e67686061644311762f8
rmit-s3740446-Ryan-Cassidy <37936511+rmit-s3740446-Ryan-Cassidy@users.noreply.github.com>
2020-03-12 18:01:22 +1100

    game functionality, csv testing

--- game.py
+++ game.py
@@ -1,5 +1,36 @@
-from sense_hat import SenseHat
+from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
 import electronicDie
+import csv
+import os
 
+sense = SenseHat()
+#Electronic die instance
 die = electronicDie.elec_die(True)
-die.prompt()
+
+#csv file
+filename = "winner.csv"
+
+#Check if file exists
+if os.path.exists(filename) == False:
+    with open(filename, 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['Winner', 'P1 Score', 'P2 Score', 'Time'])
+
+#Players
+p1 = 0
+p2 = 0
+
+sense.show_message("Game")
+while True:
+    sense.show_message("P1 Turn")
+    p1 += die.prompt()
+    print(p1)
+    if p1 >= 30:
+        break
+    sense.show_message("P2 Turn")
+    p2 += die.prompt()
+    print(p2)
+    if p2 >= 30:
+        break
+sense.show_message("End")
