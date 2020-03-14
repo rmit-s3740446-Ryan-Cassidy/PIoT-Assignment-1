6b548f8f4e8f4604cd3c94322f8cad1c21b05219
rmit-s3740446-Ryan-Cassidy <37936511+rmit-s3740446-Ryan-Cassidy@users.noreply.github.com>
2020-03-14 14:53:43 +1100

    class changes

--- game.py
+++ game.py
@@ -4,44 +4,46 @@ import csv
 import os
 import datetime
 
-sense = SenseHat()
-#Electronic die instance
-die = electronicDie.elec_die(True)
+class die_game:
+    sense = SenseHat()
+    #Electronic die instance
+    die = electronicDie.elec_die(True)
 
-#csv file
-filename = "winner.csv"
+    #csv file
+    filename = "winner.csv"
 
-#Check if file exists
-if os.path.exists(filename) == False:
-    with open(filename, 'w') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(['Winner', 'P1 Score', 'P2 Score', 'Time'])
+    #Check if file exists
+    if os.path.exists(filename) == False:
+        with open(filename, 'w') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(['Winner', 'P1 Score', 'P2 Score', 'Time'])
 
-#Players
-p1 = 0
-p2 = 0
-winner = None
-
-sense.show_message("Game")
-while True:
-    sense.show_message("P1 Turn")
-    p1 += die.prompt()
-    print(p1)
-    if p1 >= 30:
-        break
-    sense.show_message("P2 Turn")
-    p2 += die.prompt()
-    print(p2)
-    if p2 >= 30:
-        break
-if p1 > p2:
-    winner = 'p1'
-    sense.show_message("P1 is the winner")
-else:
-    sense.show_message("P2 is the winner")
-with open(filename, 'a') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow([winner, p1, p2, datetime.datetime.now()])
-sense.show_message("End")
+    #Players
+    p1 = 0
+    p2 = 0
+    winner = None
+    def start(self):
+        self.sense.show_message("Game")
+        while True:
+            self.sense.show_message("P1 Turn")
+            self.p1 += self.die.prompt()
+            print(self.p1)
+            if self.p1 >= 30:
+                break
+            self.sense.show_message("P2 Turn")
+            self.p2 += self.die.prompt()
+            print(self.p2)
+            if self.p2 >= 30:
+                break
+        if self.p1 > self.p2:
+            self.winner = 'p1'
+            self.sense.show_message("P1 is the winner")
+        else:
+            self.winner = 'p2'
+            self.sense.show_message("P2 is the winner")
+        with open(self.filename, 'a') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow([self.winner, self.p1, self.p2, datetime.datetime.now()])
+        self.sense.show_message("End")
