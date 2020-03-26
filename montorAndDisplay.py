e85483a689e541d2f86f731d2933df42ee395e36
ryanc-scalegrowth <37936511+rmit-s3740446-Ryan-Cassidy@users.noreply.github.com>
2020-03-26 21:14:36 +1100

    adjusted temp to account for cpu added comments and exit on joystick middle

--- montorAndDisplay.py
+++ montorAndDisplay.py
@@ -1,5 +1,7 @@
 import json
 import time
+import sys
+import os
 from sense_hat import SenseHat
 
 class monitor_display:
@@ -27,12 +29,22 @@ class monitor_display:
         except Exception as e:
             print(str(e))
             self.sense.clear()
+            sys.exit("Error when reading from Json. Is the Json configured correctly?")
 
     def start(self):
+        #bool for quitting
+        exit = False
         while True:
             #reading temprature from sense hat and converting to int.
-            temp = self.sense.get_temperature()
-            temp_integer_value = int(temp)
+            t1 = self.sense.get_temperature_from_humidity()
+            t2 = self.sense.get_temperature_from_pressure()
+            t_cpu = get_cpu_temp()
+
+    # Calculates the real temperature compesating CPU heating.
+            t = (t1 + t2) / 2
+            t_corr = t - ((t_cpu - t) / 1.5)
+            t_corr = get_smooth(t_corr)
+            temp_integer_value = int(t_corr)
 
             #if-else to change display color as per temperature
             if(temp_integer_value <= self.cold_max):
@@ -43,8 +55,33 @@ class monitor_display:
                 text_color = self.red
             self.sense.show_message(str(repr(temp_integer_value)),text_colour=text_color,scroll_speed=0.2)
 
+            #check joystick events, exit if middle is pressed
+            for event in self.sense.stick.get_events():
+                if event.direction == 'middle' and event.action == 'released':
+                    exit = True
+            if exit == True:
+                break
+            
             #waiting for ten seconds then reading temp again
             time.sleep(10)
+
+# Get CPU temperature.
+def get_cpu_temp():
+    res = os.popen("vcgencmd measure_temp").readline()
+    return float(res.replace("temp=","").replace("'C\n",""))
+
+# Use moving average to smooth readings.
+def get_smooth(x):
+    if not hasattr(get_smooth, "t"):
+        get_smooth.t = [x,x,x]
+    
+    get_smooth.t[2] = get_smooth.t[1]
+    get_smooth.t[1] = get_smooth.t[0]
+    get_smooth.t[0] = x
+
+    return (get_smooth.t[0] + get_smooth.t[1] + get_smooth.t[2]) / 3
+
+
 
 #Standalone testing
 if __name__ == '__main__':
